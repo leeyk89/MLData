b5810223892a83b8e5503e55e7ffbb8c59b2bddd
DevYG <yangguy019@naver.com>
2020-08-19 12:10:39 +0900

    ipynb용 맷플로립 제거

--- Titanic.py
+++ Titanic.py
@@ -1,22 +1,15 @@
-
 # coding: utf-8
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-#.맷플로립를 이용하여 ipython에 그래프 보이게 하기 위해 선언.
-%matplotlib inline
 
 
 titanic_df = pd.read_csv('./Titanic_Data/train.csv')
 titanic_df.head(10)
 
-
 print('학습 정보 열람하기')
 print(titanic_df.info())
-
-
-
 
 #sklearn에서는 null 값을 허용하지 않으므로 현재 결손값을 어떻게 처리할지 결정해야합니다.
 titanic_df['Age'].fillna(titanic_df['Age'].mean(),inplace=True)
@@ -24,19 +17,12 @@ titanic_df['Cabin'].fillna('N',inplace=True)
 titanic_df['Embarked'].fillna('N',inplace=True)
 print('데이터 결손값 개수 = ',titanic_df.isnull().sum().sum())
 
-
 titanic_df['Cabin']=titanic_df['Cabin'].str[:1]
 print(titanic_df['Cabin'].head(3))
 
-
-
 sns.barplot(x='Sex',y='Survived',data=titanic_df)
 
-
-
 sns.barplot(x='Pclass',hue='Sex',y='Survived',data=titanic_df)
-
-
 
 # 입력 age에 따라 구분값을 반환하는 함수 설정. DataFrame의 apply lambda식에 사용. 
 def get_category(age):
@@ -63,7 +49,3 @@ group_names = ['Unknown', 'Baby', 'Child', 'Teenager', 'Student', 'Young Adult',
 titanic_df['Age_cat'] = titanic_df['Age'].apply(lambda x : get_category(x))
 sns.barplot(x='Age_cat', y = 'Survived', hue='Sex', data=titanic_df, order=group_names)
 titanic_df.drop('Age_cat', axis=1, inplace=True)
-
-
-
-
